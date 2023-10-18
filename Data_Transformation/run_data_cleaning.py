ee41fd92be4d46baba7a8a1e6878164939049fb5
emilyng-sz <emilyng.sz831@gmail.com>
2023-10-18 13:16:46 +0800

    complete run_data_cleaning.py scripts

--- Data_Transformation/run_data_cleaning.py
+++ Data_Transformation/run_data_cleaning.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from data_helper import *
 
 # Check if the 'Data' folder exists, and create it if not.
 # Note that Data folder is untracked by git, hence data files can be added here
@@ -7,3 +8,21 @@ data_folder = 'Data'
 if not os.path.exists(data_folder):
     os.mkdir(data_folder)
 
+## Add csv files into the Data folder for the below steps to work
+
+data_folder = 'Data/Cleaned'
+if not os.path.exists(data_folder):
+    os.mkdir(data_folder)
+
+
+file_paths = ['../Data/raw_Cp5_a.csv',
+              '../Data/raw_Cp33a45b6b_a.csv',
+              '../Data/Sample raw data format - duration_cp10_Apr2023_a.csv']
+
+for file_path in file_paths:
+    df_clean = clean_carpark_data(file_path)
+    df_clean_final = generate_duration(generate_dow(df_clean))
+
+    # Save the cleaned data to a csv file
+    file_name = file_path.split('/')[-1].split('.')[0]
+    df_clean_final.to_csv(os.path.join('../',data_folder, file_name + '_cleaned' + '.csv'), index=False)
