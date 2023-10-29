48a04069b50a9d967b38eb1310becf0205d0536d
hoofangyu <e0726391@u.nus.edu>
2023-10-29 22:24:55 +0800

    Update app.py and home.py

--- frontend/app.py
+++ frontend/app.py
@@ -7,6 +7,14 @@ import pandas as pd
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+custom_css = """
+    body, html, #content {
+        margin: 0;
+        padding: 0;
+    }
+"""
+
+
 #Create Navigation Bar
 navbar = dbc.Navbar(
             dbc.Container(
@@ -14,8 +22,8 @@ navbar = dbc.Navbar(
                     # Create Logo and Brand flushed to the left
                     dbc.Row(
                         dbc.Col([
-                            html.Img(src=dash.get_asset_url('nus_logo.svg'), height="72px", style={"padding":"10px", 'vertical-align':'middle'}),
-                            dbc.NavbarBrand("Parkitect", style = {'font-weight':'bold','font-size':'36px', 'color':'#3A3B3C','padding':'30px','vertical-align':'middle'})
+                            html.Img(src=dash.get_asset_url('nus_logo.svg'), height="72px", style={"padding":"10px"}),
+                            dbc.NavbarBrand("Parkitect", style = {'font-weight':'bold','font-size':'36px', 'color':'#3A3B3C','padding':'0px 30px','vertical-align':'middle'})
                                 ])
 
                     ),
@@ -24,16 +32,16 @@ navbar = dbc.Navbar(
                     dbc.Row(      
                         dbc.Col(
                             dbc.Nav([
-                                dbc.NavItem(dbc.NavLink("Home", href = "/",style ={'color':'#3A3B3C'})),
-                                dbc.NavItem(dbc.NavLink("App", href = "/parkapp",style={'color':'#3A3B3C'})),
-                                dbc.NavItem(dbc.NavLink("Tutorial", href = "/tutorial",style ={'color':'#3A3B3C'}))
+                                dbc.NavItem(dbc.NavLink("Home", href = "/",style ={'color':'#3A3B3C', 'text-align':'center'}),style={'border-style': 'solid','margin-right':'15px','width':'150px','border-radius': '25px'}),
+                                dbc.NavItem(dbc.NavLink("App", href = "/parkapp",style={'color':'#3A3B3C', 'text-align':'center'}),style={'border-style': 'solid', 'margin-right':'15px','width':'150px','border-radius': '25px'}),
+                                dbc.NavItem(dbc.NavLink("Tutorial", href = "/tutorial",style ={'color':'#3A3B3C', 'text-align':'center'}),style={'border-style': 'solid', 'margin-right':'15px','width':'150px','border-radius': '25px'})
                             ])
                         )
                     )
                 ], 
-            style = {'font-family': 'Open Sans', 'font-size': '36px'},
+            style = {'font-family': 'Open Sans', 'font-size': '18px', 'height':'auto','background-color':'#d3d3d3'},
             fluid=True
-            )
+            ), style = {'padding':'0px'} #set to 0 else will have white space
 )
 
 
--- frontend/pages/home.py
+++ frontend/pages/home.py
@@ -8,9 +8,10 @@ layout = html.Div(
     html.H1("PARKitect: Designing Smart Parking Solutions"),
     html.H2("Your one-stop NUS parking simulation system"),
     html.Br(),
-    html.P("*Red Lots - Strictly for staff only"),
-    html.P("*White Lots - For everybody")
+    html.P("*Red Lots - Strictly for staff only", style = {'margin-bottom':'0px', "color": "red"}),
+    html.P("*White Lots - For everybody", style= {'margin-bottom':'0px'})
     ], 
     # Centralise text chunk vertically and horizontally
-    style={'font-family': 'Open Sans','text-align':'center','position': 'absolute', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)'}
+    style={'font-family': 'Open Sans','text-align':'center','position': 'absolute', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)','white-space': 'nowrap'}
     )
+
