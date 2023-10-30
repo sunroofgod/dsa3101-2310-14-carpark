a53d95a3b31bae6520c8f1e866dbe6f9a72d849c
mingda2000 <teo.mingda@gmail.com>
2023-10-30 19:38:48 +0800

    Update select date to select month

--- frontend/pages/parkapp.py
+++ frontend/pages/parkapp.py
@@ -5,6 +5,7 @@ from dash import html, dcc
 dash.register_page(__name__) #signifies homepage
 
 campus_events = ['No Event','First Week New AY', 'Well-Being Day', 'Commencement', 'Examinations', 'Staff WFH Day', 'Rag & Flag Day', 'SuperNova', 'Open Day']
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 layout = dbc.Container([
     dbc.Row([
@@ -16,9 +17,12 @@ layout = dbc.Container([
             ),
         dbc.Col(html.Img(src=dash.get_asset_url('nus_map.png'), style = {'width':'95%','height':'auto'}),width = 8 ,style = {'display': 'flex', 'justify-content': 'center', 'align-items':'center', 'padding':'10px 0px'}),
         dbc.Col([
-            html.Div([html.Div(html.B("Select Date")),
-            html.Div(dcc.DatePickerSingle(id='date-picker', date='2023-01-01'))]),
+            html.Div([html.Div(html.B("Select Month")),
+            html.Div(dcc.Dropdown(id = 'event-picker', options=months, value = "Jan", clearable = False, style = {'font-size':'15px'}))]),
             html.Br(),
+            #html.Div([html.Div(html.B("Select Date")),
+            #html.Div(dcc.DatePickerSingle(id='date-picker', date='2023-01-01'))]),
+            #html.Br(),
             #html.Div([html.Div(html.B("Select Time")),
             #html.Div(dcc.Input(id="time-picker", type="text", placeholder="hh:mm:ss", style={'text-align':'center', 'width':'150px'}))]),
             #html.Br(),
