c1d63702d992a5ca3e8a60cc887377c11da62ac2
hoofangyu <e0726391@u.nus.edu>
2023-10-30 10:15:43 +0800

    Updated parkapp.py

--- frontend/pages/parkapp.py
+++ frontend/pages/parkapp.py
@@ -4,5 +4,5 @@ from dash import html
 dash.register_page(__name__) #signifies homepage
 
 layout = html.Div([
-    html.H1("App page")
-])
+    html.Img(src=dash.get_asset_url('nus_map.png'), style = {'width':'65%','height':'auto'})
+], style = {'display': 'flex', 'justify-content': 'center', 'align-items':'center','padding':'10px'})
