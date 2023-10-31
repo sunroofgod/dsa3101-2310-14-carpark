d184a8859a0ee8d5c03b6b04a24dcaedc9422ab1
mingda2000 <teo.mingda@gmail.com>
2023-10-31 22:29:07 +0800

    Updated coordinates of every carpark with their respective labels

--- frontend/pages/parkapp.py
+++ frontend/pages/parkapp.py
@@ -7,6 +7,7 @@ dash.register_page(__name__) #signifies homepage
 campus_events = ['No Event','First Week New AY', 'Well-Being Day', 'Commencement', 'Examinations', 'Staff WFH Day', 'Rag & Flag Day', 'SuperNova', 'Open Day']
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
+
 layout = dbc.Container([
     dbc.Row([
         dbc.Col(
@@ -15,17 +16,54 @@ layout = dbc.Container([
             ]
             ),style = {'text-align':'center','background-color':'#003d7c','padding-top':'10px','color':'#FFFFFF'}
             ),
-        dbc.Col(html.Img(src=dash.get_asset_url('nus_map.png'), style = {'width':'95%','height':'auto'}),width = 8 ,style = {'display': 'flex', 'justify-content': 'center', 'align-items':'center', 'padding':'10px 0px'}),
+        dbc.Col(
+            html.Div([
+            html.Img(src=dash.get_asset_url('nus_map.png'), style={'width': '95%', 'height': 'auto'}),
+            html.Div([
+                html.Div('●', id='cp3', style={'position': 'absolute', 'top': '30px', 'left': '210px', 'color': 'lightgreen', 'font-size': '90px'}),
+                html.Div('CP3', style = {'position': 'absolute', 'top': '60px', 'left': '218px', 'font-weight': 'bold'}),
+                ]
+                ),
+            html.Div([
+                html.Div('●', id='cp3a', style={'position': 'absolute', 'top': '40px', 'left': '250px', 'color': 'lightgreen', 'font-size': '90px'}),
+                html.Div('CP3A', style = {'position': 'absolute', 'top': '70px', 'left': '255px', 'font-weight': 'bold'}),
+                ]
+                ),
+            html.Div([
+                html.Div('●', id='cp4', style={'position': 'absolute', 'top': '110px', 'left': '260px', 'color': 'lightgreen', 'font-size': '90px'}),
+                html.Div('CP4', style = {'position': 'absolute', 'top': '200px', 'left': '270px', 'font-weight': 'bold'}),
+                ]
+                ),
+            html.Div([
+                html.Div('●', id='cp5', style={'position': 'absolute', 'top': '120px', 'left': '330px', 'color': 'lightgreen', 'font-size': '90px'}),
+                html.Div('CP5', style = {'position': 'absolute', 'top': '210px', 'left': '340px', 'font-weight': 'bold'}),
+                ]
+                ),
+            html.Div([
+                html.Div('●', id='cp5b', style={'position': 'absolute', 'top': '80px', 'left': '310px', 'color': 'lightgreen', 'font-size': '90px'}),
+                html.Div('CP5B', style = {'position': 'absolute', 'top': '110px', 'left': '315px', 'font-weight': 'bold'}),
+                ]
+                ),
+            html.Div([
+                html.Div('●', id='cp6b', style={'position': 'absolute', 'bottom': '80px', 'right': '250px', 'color': 'lightgreen', 'font-size': '90px'}),
+                html.Div('CP6B', style = {'position': 'absolute', 'bottom': '95px', 'right': '255px', 'font-weight': 'bold'}),
+                ]
+                ),
+            html.Div([
+                html.Div('●', id='cp10', style={'position': 'absolute', 'bottom': '162px', 'right': '130px', 'color': 'lightgreen', 'font-size': '90px'}),
+                html.Div('CP10', style = {'position': 'absolute', 'bottom': '240px', 'right': '135px', 'font-weight': 'bold'}),
+                ]
+                ),
+                ],
+                style={'position': 'relative'}
+            ),
+            width=8,
+            style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'padding': '10px 0px'}
+            ),
         dbc.Col([
             html.Div([html.Div(html.B("Select Month")),
             html.Div(dcc.Dropdown(id = 'event-picker', options=months, value = "January", clearable = False ))]),
             html.Br(),
-            #html.Div([html.Div(html.B("Select Date")),
-            #html.Div(dcc.DatePickerSingle(id='date-picker', date='2023-01-01'))]),
-            #html.Br(),
-            #html.Div([html.Div(html.B("Select Time")),
-            #html.Div(dcc.Input(id="time-picker", type="text", placeholder="hh:mm:ss", style={'text-align':'center', 'width':'150px'}))]),
-            #html.Br(),
             html.Div([html.Div(html.B("Select Event")),
             html.Div(dcc.Dropdown(id = 'event-picker', options=campus_events, value = "No Event", clearable = False))]),
             html.Br(),
