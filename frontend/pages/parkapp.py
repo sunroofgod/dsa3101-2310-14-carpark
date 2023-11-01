6474ff97dee3de6eb13b112c3aa4336ad08f4db5
hoofangyu <e0726391@u.nus.edu>
2023-11-01 13:19:35 +0800

    Update parkapp.py cp buttons and locations

--- frontend/pages/parkapp.py
+++ frontend/pages/parkapp.py
@@ -15,44 +15,44 @@ layout = dbc.Container([
             html.Div([
                 html.B("Currently Simulating:")
             ]
-            ),style = {'text-align':'center','background-color':'#003d7c','padding-top':'2vh','color':'#FFFFFF'}
+            ),style = {'text-align':'center','background-color':'#003d7c','padding-top':'1%','color':'#FFFFFF'}
             ),
         dbc.Col(
             html.Div([
             html.Div(html.Img(src=dash.get_asset_url('nus_map.png'), style={'width': '100%', 'height': 'auto'}), style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'}),
             html.Div([
                 html.Button('100', id='cp3', className = 'cp-button', style = {'top':'16%', 'left':'27%'}),
-                html.Div('CP3', style = {'position': 'absolute', 'top': '12%', 'left': '27.7%', 'font-weight': 'bold'}),
+                html.Div('CP3', style = {'position': 'absolute', 'top': '12%', 'left': '27.7%', 'font-weight': 'bold'})
                 ]
                 ),
             html.Div([
-                #html.Div('●', id='cp3a', style={'position': 'absolute', 'top': '40px', 'left': '250px', 'color': 'lightgreen', 'font-size': '90px'}),
-                #html.Div('CP3A', style = {'position': 'absolute', 'top': '70px', 'left': '255px', 'font-weight': 'bold'}),
+                html.Button('100', id='cp3a', className = 'cp-button', style = {'top':'17%', 'left':'32%'}),
+                html.Div('CP3A', style = {'position': 'absolute', 'top': '13%', 'left': '32%', 'font-weight': 'bold'})
                 ]
                 ),
             html.Div([
-                #html.Div('●', id='cp4', style={'position': 'absolute', 'top': '110px', 'left': '260px', 'color': 'lightgreen', 'font-size': '90px'}),
-                #html.Div('CP4', style = {'position': 'absolute', 'top': '200px', 'left': '270px', 'font-weight': 'bold'}),
+                html.Button('100', id='cp4', className = 'cp-button', style = {'top':'32%', 'left':'34%'}),
+                html.Div('CP4', style = {'position': 'absolute', 'top': '39%', 'left': '34.7%', 'font-weight': 'bold'})
                 ]
                 ),
             html.Div([
-                #html.Div('●', id='cp5', style={'position': 'absolute', 'top': '120px', 'left': '330px', 'color': 'lightgreen', 'font-size': '90px'}),
-                #html.Div('CP5', style = {'position': 'absolute', 'top': '210px', 'left': '340px', 'font-weight': 'bold'}),
+                html.Button('100', id='cp5', className = 'cp-button',style = {'top':'34%', 'left':'43%'}),
+                html.Div('CP5', style = {'position': 'absolute', 'top': '41%', 'left': '43.7%', 'font-weight': 'bold'})
                 ]
                 ),
             html.Div([
-                #html.Div('●', id='cp5b', style={'position': 'absolute', 'top': '80px', 'left': '310px', 'color': 'lightgreen', 'font-size': '90px'}),
-                #html.Div('CP5B', style = {'position': 'absolute', 'top': '110px', 'left': '315px', 'font-weight': 'bold'}),
+                html.Button('100', id='cp5b', className = 'cp-button', style = {'top':'26%', 'left':'42%'}),
+                html.Div('CP5B', style = {'position': 'absolute', 'top': '22%', 'left': '42%', 'font-weight': 'bold'})
                 ]
                 ),
             html.Div([
-                html.Div('●', id='cp6b', style={'position': 'absolute', 'bottom': '80px', 'right': '250px', 'color': 'lightgreen', 'font-size': '90px'}),
-                html.Div('CP6B', style = {'position': 'absolute', 'bottom': '95px', 'right': '255px', 'font-weight': 'bold'}),
+                html.Button('100', id='cp6b', className = 'cp-button', style = {'top':'62%', 'left':'62%'}),
+                html.Div('CP6B', style = {'position': 'absolute', 'top': '69%', 'left': '62.2%', 'font-weight': 'bold'})
                 ]
                 ),
             html.Div([
-                html.Div('●', id='cp10', style={'position': 'absolute', 'bottom': '162px', 'right': '130px', 'color': 'lightgreen', 'font-size': '90px'}),
-                html.Div('CP10', style = {'position': 'absolute', 'bottom': '240px', 'right': '135px', 'font-weight': 'bold'}),
+                html.Button('100', id='cp10', className = 'cp-button', style = {'top':'53%', 'left':'84%'}),
+                html.Div('CP10', style = {'position': 'absolute', 'top': '60%', 'left': '84.2%', 'font-weight': 'bold'})
                 ]
                 ),
                 ],
