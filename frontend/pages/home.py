325310ecf25e2254ef9caa045497cb6a195964e9
Jason <jasoncz2000@gmail.com>
2023-11-03 17:58:31 +0800

    Changed colour of words for homepage and sizing of Title

--- frontend/pages/home.py
+++ frontend/pages/home.py
@@ -3,15 +3,28 @@ from dash import html
 
 dash.register_page(__name__, path = '/') #signifies homepage
 
+container = html.Div(
+    [
+        html.H1("PARKitect:", style={'font-weight': 'bold', 'color': 'blue', 'display': 'inline'}),
+        html.H1(" Designing Smart Parking Solutions", style={'font-weight': 'bold', 'color': 'orange', 'display': 'inline'}),
+        html.H2("Your one-stop NUS parking simulation system", style={'font-weight': 'bold'}),
+        html.Br(),
+        html.P("*Red Lots - Strictly for staff only", style={'margin-bottom': '0px', "color": "red"}),
+        html.P("*White Lots - For everybody", style={'margin-bottom': '0px'})
+    ],
+    style={
+        'text-align': 'center',
+        'border': '5px none',  # Rectangular border
+        'border-radius': '15px',  # Adjust the border-radius to make the corners rounded
+        'padding': '20px'  # Add some padding for spacing between the text and the border
+    }
+)
+
 layout = html.Div(
     [
-    html.H1("PARKitect: Designing Smart Parking Solutions", style={'font-weight':'bold'}),
-    html.H2("Your one-stop NUS parking simulation system", style={'font-weight':'bold'}),
-    html.Br(),
-    html.P("*Red Lots - Strictly for staff only", style = {'margin-bottom':'0px', "color": "red"}),
-    html.P("*White Lots - For everybody", style= {'margin-bottom':'0px'})
-    ], 
-    # Centralise text chunk vertically and horizontally
-    style={'font-family': 'Open Sans','text-align':'center','position': 'absolute', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)','white-space': 'nowrap'}
-    )
+        container
+    ],
+    # Centralize the container vertically and horizontally
+    style={'position': 'absolute', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)', 'white-space': 'nowrap'}
+)
 
