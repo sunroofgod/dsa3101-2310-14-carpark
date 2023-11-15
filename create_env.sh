84c4642bed19cb79eb48ca3cd84c49b59ec86810
Zhan Yi <liauzhanyi@gmail.com>
2023-11-16 01:22:58 +0800

    Delete redundant docker files

--- create_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "DATABASE_NAME=NUS-carpark-data" > .env
-echo "MYSQL_ROOT_PASSWORD=root_password" >> .env
-echo "MYSQL_USERNAME=root" >> .env
-
-echo ".env file created successfully."
